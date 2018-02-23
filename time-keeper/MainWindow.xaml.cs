d64afe613a9ebeaba1d67a58fb87c2e2ed8b4e71
Steven Forrest <stvn910@gmail.com>
2018-02-23 12:14:47 -0500

    Remove uneeded usings

--- time-keeper/MainWindow.xaml.cs
+++ time-keeper/MainWindow.xaml.cs
@@ -1,26 +1,12 @@
 ﻿using MahApps.Metro.Controls;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Mime;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
 using System.Timers;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using DateTime = System.DateTime;
-using iTextSharp;
 using iTextSharp.text;
 using iTextSharp.text.pdf;
-using System.Diagnostics;
 using System.IO;
 
 namespace time_keeper
